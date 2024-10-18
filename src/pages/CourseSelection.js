ba4a77c742394a5c8d175ea91d2fd437a54fb30e
nbailey04 <nathanbailey584@gmail.com>
2024-10-17 22:07:09 -0400

    Updated Top Margin on CourseSelection.js
    Added some space to the top margin for format purposes.

--- src/pages/CourseSelection.js
+++ src/pages/CourseSelection.js
@@ -4,7 +4,7 @@ import React from 'react';
 const courses = ['C++', 'Java', 'Python', 'JavaScript'];
 
 const CourseSelection = () => (
-  <div className="courses-grid">
+  <div className="courses-grid" style={{ marginTop: '50px' }}>
     {courses.map((course) => (
       <div className="course-tile" key={course}>
         {course}
